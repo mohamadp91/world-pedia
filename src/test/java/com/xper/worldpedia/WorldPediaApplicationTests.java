69c89b034864b2848c7ae06fea8d493cfb8ea8eb
mohamadp91 <mohamad7291m@gmail.com>
2021-09-14 15:26:06 +0430

    initial the world pedia project

---
+++ src/test/java/com/xper/worldpedia/WorldPediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xper.worldpedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldPediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
