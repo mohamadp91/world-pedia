19cdbe2b755a2e7b029f20d1b007a713bddd9202
mohamadp91 <69380537+mohamadp91@users.noreply.github.com>
2021-09-14 15:30:10 +0430

    initial the world pedia project (#1)

---
+++ src/main/java/com/xper/worldpedia/WorldPediaApplication.java
@@ -0,0 +1,13 @@
+package com.xper.worldpedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldPediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldPediaApplication.class, args);
+    }
+
+}
